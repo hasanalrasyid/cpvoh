5196d9c5e6a1f5ecc4f93376032ac8c1beb4efda
Hasan <hasanalrasyid.har@gmail.com>
2018-07-25 15:59:07 +0900

    ok until 2nd atoms....

--- app/zmat2cart.hs
+++ app/zmat2cart.hs
@@ -15,7 +15,7 @@ import Language.Fortran.Parser.Utils (readReal)
 import Data.Maybe (fromJust)
 import qualified Data.Map.Strict as M (fromList,lookup,Map)
 import Data.Tree (Tree(Node),drawTree)
-import Data.Sequence (fromList,(|>),Seq)
+import Data.Sequence (fromList,(|>),Seq,index)
 import Data.Foldable (toList)
 import qualified Numeric.LinearAlgebra as HM (norm_2,scale)
 import qualified Numeric.LinearAlgebra.Data as HM (fromList, Vector)
@@ -25,7 +25,7 @@ main = do
     opts <- execParser withHelp
     z <- getContents
     let vInit0 = HM.fromList [0,0,0] :: HM.Vector Double
-    let dInit@(dInitDih,vInit1) =  (180, HM.fromList [3,2,0]) :: (Double,HM.Vector Double)
+    let dInit@(dInitDih,vInit1) =  (180, HM.fromList [1,0,0]) :: (Double,HM.Vector Double)
     putStrLn $ show opts
     let [struct,vars] = map (filter (not . null)) $ splitOn [["Variables:"]] $ drop 5 $ map words $ lines $ z
         varMap = M.fromList $ map (\[a,b] -> (a, fromJust $ readReal b)) vars
@@ -40,11 +40,19 @@ genCart vMap v0 d1@(dih,v1) (x:xs) res = genCart vMap v0 d1 xs $ res |> fromZMat
   where
     fromZMat :: [String] -> (String, HM.Vector Double)
     fromZMat [s1] = (s1,v0)
-    fromZMat [s2a,s2b,s2c] = (s2a, HM.scale ( callVar s2c) $ vUnity v1)
-    fromZMat [s3a,s3b,s3c,s3d,s3e] = (s3a,HM.fromList [2,0,0])
-    fromZMat (sna:_) = (sna,HM.fromList [9,0,0])
+    fromZMat [s2a,_,r2] = (s2a, HM.scale ( callVar r2) $ vUnity v1)
+    fromZMat [s3a,r3ref,r3,a3ref,a3] = let ca3 = callVar a3
+                                           cr3 = callVar r3
+                                           vCm1 = snd $ index res 1
+                                        in (s3a,vCm1 + HM.scale cr3 ( HM.fromList [cos ca3,sin ca3 ,0] ))
+    fromZMat (sNa:rNref:rNs:aNref:aNs:dNref:dNs:_) =
+      let [rN,aN,dN] = map callVar [rNs,aNs,dNs]
+          [crr,cra,crd] = map (snd . (index res) . floor . callVar) [rNref,aNref,dNref]
+       in (sNa, HM.fromList [rN, aN, dN] )
+    --fromZMat (sna:_) = (sna,HM.fromList [9,0,0])
     vUnity :: HM.Vector Double -> HM.Vector Double
     vUnity v = HM.scale (1/(HM.norm_2 v)) v
+    callVar :: String -> Double
     callVar s = fromJust $ M.lookup s vMap
 data Opts = Opts {
     _outFormat    :: String,
