973d5e0218f65b9cf8ebebc0d278e204d0b29712
Hasan <hasanalrasyid.har@gmail.com>
2018-11-16 16:58:05 +0900

    inisiasi

--- app/f1PdosTotAtomicMajorOrbitalVert.hs
+++ app/f1PdosTotAtomicMajorOrbitalVert.hs
@@ -16,13 +16,22 @@ main = do
                  , "extendedNiCo2O4.normal/nico2o4.1G0"
                  , "Ni:Ni#3d:6:7:9:8:10", "Co:Co#3d:6:7:8:9:10", "O:O#2p:3:4:5"
                  ]
-  let testArgs = [ "QSGW_{0}"
+  let testArgsOld = [ "QSGW_{0}"
                  , "-9:2", "55", "T"
                  , "o", "1"
                  , "nico2o4"
                  , "extendedNiCo2O4.normal/nico2o4.0GGA"
                  ,"Ni:Ni#3d:6:7:9:8:10", "Co:Co#3d:6:7:8:9:10", "O:O#2p:3:4:5"
                  ]
+  let testArgs = [ "QSGW_{0}"
+                 , "-9:2", "55", "T"
+                 , "o", "1"
+                 , "nico2o4"
+                 ,"Ni:Ni#3d:6:7:9:8:10/Co:Co#3d:6:7:8:9:10/O:O#2p:3:4:5"
+                 , "extendedNiCo2O4.normal/nico2o4.0GGA"
+                 , "extendedNiCo2O4.normal/nico2o4.0GW"
+                 , "extendedNiCo2O4.normal/nico2o4.11GW20"
+                 ]
   -}
-  plotPDOS allArgs
+--  plotPDOS allArgs
   putStrLn "========beres======="
