79ca1e2721848438fc39edaaca861e6b33b29088
Hasan <hasanalrasyid.har@gmail.com>
2018-11-26 14:34:55 +0900

    pdos

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
-echo "=====TEST: f2BandStructure"
-~/kanazawa/dev/cpvoh/app/f2BandStructure.hs -o tempor/kadal -b -y "-9:9" -t "QSGW#One Shot QSGW#GGA" ext.nico2o4.normal/nico2o4.6G10:1:"11G20.Majority"#ext.nico2o4.normal/nico2o4.6G10:2:"11G20.Minority" ext.nico2o4.normal/nico2o4.1G0:1:"G0.Majority"#ext.nico2o4.normal/nico2o4.1G0:2:"G0.Minority" ext.nico2o4.normal/nico2o4.GGA0:1:Majority#ext.nico2o4.normal/nico2o4.GGA0:2:Minority
+#echo "=====TEST: f2BandStructure"
+#~/kanazawa/dev/cpvoh/app/f2BandStructure.hs -o tempor/kadal -b -y "-9:9" -t "QSGW#One Shot QSGW#GGA" ext.nico2o4.normal/nico2o4.6G10:1:"11G20.Majority"#ext.nico2o4.normal/nico2o4.6G10:2:"11G20.Minority" ext.nico2o4.normal/nico2o4.1G0:1:"G0.Majority"#ext.nico2o4.normal/nico2o4.1G0:2:"G0.Minority" ext.nico2o4.normal/nico2o4.GGA0:1:Majority#ext.nico2o4.normal/nico2o4.GGA0:2:Minority
+
+echo "=====TEST: f1PDOS"
+~/kanazawa/dev/cpvoh/app/f1PDOS.hs -o tempor/kadal -b -x "-8:3" -y "-9:9" -t "QSGW#One Shot QSGW#GGA" ext.nico2o4.normal/nico2o4.6G10:1:"11G20.Majority"#ext.nico2o4.normal/nico2o4.6G10:2:"11G20.Minority" ext.nico2o4.normal/nico2o4.1G0:1:"G0.Majority"#ext.nico2o4.normal/nico2o4.1G0:2:"G0.Minority" ext.nico2o4.normal/nico2o4.GGA0:1:Majority#ext.nico2o4.normal/nico2o4.GGA0:2:Minority
 
