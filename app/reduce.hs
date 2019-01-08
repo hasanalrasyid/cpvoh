46a814f66dae0ccabba13402a60348e49a9e315f
Hasan <hasanalrasyid.har@gmail.com>
2019-01-08 16:29:53 +0900

    numbering the line using Mealy

--- app/reduce.hs
+++ app/reduce.hs
@@ -21,29 +21,42 @@ import Data.ByteString (ByteString)
 import System.IO (isEOF)
 import Control.Monad (unless)
 
+import Data.Machine.Mealy
+
 main :: IO ()
 main = do
-  arah' <- getArgs
-  let arah = 3 |>  (map read arah' :: [Double])
-  res <- runT (pipeline arah)
+--  res <- runT pipeline
   -- Remember that runT collects outputs in a list
-  putStrLn $ format "  " (printf "%.6f") $ fromRows res
-  where pipeline t = lineSource ~> genVec ~> moveVec t
+--  putStrLn $ format "  " (printf "%.6f") $ fromRows res
+  res <- runT $ lineSource ~> auto lineNum ~> skipHeader 2 ~> echo
+  putStrLn $ show res
+  --where pipeline = lineSource ~> skipHeader ~> genVec ~> genLat ~> echo
 
-lineSource :: MonadIO m => SourceT m ByteString
-lineSource = ioSource isEOF BS.getLine
+lineNum = unfoldMealy (\i x -> ((i, x), i + 1)) 0
 
-ioSource :: MonadIO m => IO Bool -> IO a -> SourceT m a
-ioSource k f = construct go where
-  go = do
-    cond <- liftIO k
-    unless cond $ do
-      liftIO f >>= yield >> go
+
+--genLat :: Monad m => ProcessT m (Vector Double) ([Vector Double],Vector Double)
+genLat :: Monad m => ProcessT m a a
+genLat = dropping 3
 
 genVec :: Monad m => ProcessT m BS.ByteString (Vector Double)
 genVec = repeatedly $ do
   line <- await
   yield $ fromList $ map read $ words $ B8.unpack line
+
+skipHeader :: Monad m => Int -> ProcessT m a a
+skipHeader n = dropping n
+
+--lineNum :: Monad m => ProcessT m BS.ByteString (Int, BS.ByteString)
+
+--lineSource :: MonadIO m => SourceT m ByteString
+lineSource = ioSource isEOF BS.getLine
+
+ioSource k f = construct go where
+  go = do
+    cond <- liftIO k
+    unless cond $ do
+      liftIO f >>= yield >> go
 
 moveVec :: Monad m => Vector Double -> ProcessT m (Vector Double) (Vector Double)
 moveVec v = repeatedly $ do
